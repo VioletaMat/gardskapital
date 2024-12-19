78858db6dbc0d8497f632ad445535c4c0a8db9a4
Violeta Matovic <violeta.matovic94@gmail.com>
2024-12-19 19:49:33 +0100

    Fixing design and implementing new functionalities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import logo from '../assets/icons/icon.svg'
 import '../styles/Navbar.css'
-export const Navbar = () => {
+export const Navbar = ({ isBlackText }) => {
     const { t, i18n } = useTranslation();
 
     // scroll event listener
@@ -17,12 +17,19 @@ export const Navbar = () => {
         navbar.classList.remove("navbar-dark-scrolled");
       }
     });
+ 
+      const isLanguageSelected = (lang) => i18n.language === lang;
 
     return (
-      <nav id="navbar" className="navbar navbar-expand-lg navbar-dark fixed-top">
+         <nav
+          id="navbar"
+          className={`navbar navbar-expand-lg navbar-dark fixed-top ${isBlackText ? 'navbar-black-text' : ''}`} 
+        >
         <div className="container">
-          <div className=''>
-            <img src={logo} style={{maxWidth:'75%',height:'auto'}} alt="Responsive image"/>
+          <div >
+            <img src={logo} style={{maxWidth:'75%',height:'auto'}}
+               className={isBlackText ? 'logo-black-text' : ''} 
+           alt="Responsive image"/>
           </div>
           <div className="ml-auto" id="navbarNav">
             <ul className="navbar-nav">
@@ -47,8 +54,9 @@ export const Navbar = () => {
         </div>
 
           <div className="ml-auto" >
-            <button className="btn text-light text-uppercase" onClick={() => i18n.changeLanguage('en')}>{t('navbar.localisation1')}</button>
-            <button className="btn text-light text-uppercase" onClick={() => i18n.changeLanguage('sv')}>{t('navbar.localisation2')}</button>
+            <button className={`btn text-light text-uppercase ${isLanguageSelected('en') ? 'font-weight-bold' : ''}`}
+             onClick={() => i18n.changeLanguage('en')}>{t('navbar.localisation1')}</button>
+            <button className={`btn text-light text-uppercase ${isLanguageSelected('sv') ? 'font-weight-bold' : ''}`} onClick={() => i18n.changeLanguage('sv')}>{t('navbar.localisation2')}</button>
           </div>
         </div>
       </nav>      
